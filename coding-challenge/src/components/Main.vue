0c93c7b1cd05a78fa6945c3bae6c2b28fb1ee992
Anny Zahid <annyzahid92@gmail.com>
2023-01-02 13:18:33 +0100

    task: added and removed unnecessary code

---
+++ coding-challenge/src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Online Shopping from Local and International Market</h3>
+    <p>Message is: {{ message }}</p>
+    <input v-model="message" placeholder="edit me" />
+  </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
