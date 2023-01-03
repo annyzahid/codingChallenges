d52386b296245e307c2081d6fa5a42e92d596915
Anny Zahid <annyzahid92@gmail.com>
2023-01-03 11:13:16 +0100

    task: added UI for inputs

--- coding-challenge/src/components/Main.vue
+++ coding-challenge/src/components/Main.vue
@@ -17,63 +17,235 @@
         </p>
       </v-col>
     </v-row>
-    <v-container class="grey lighten-2">
+    <!-- Input Container for Books -->
+    <v-container class="grey lighten-2" style="height: 11rem;width: 70%">
       <v-row class="mb-6" no-gutters>
         <v-col md="2">
-          <h2 style="margin-top: 3rem"> Book </h2>
+          <h2 style="margin-top: 3rem">Book</h2>
         </v-col>
-        <v-col md="2">
-          <h3> Price </h3>
-          <h4 style="margin-top: 1.3rem"> {{ book.price }} </h4>
-        </v-col>
-        <v-col md="4" >
+        <v-col md="4">
           <v-row class="mb-6" no-gutters>
-            <v-col md="4" style="margin-bottom: -4rem">
-              <v-container class="px-0" fluid>
-                <v-checkbox
-                  v-model="book.imported"
-                  :label="`Imported`"
-                ></v-checkbox>
-              </v-container>
-            </v-col>
-            <v-col md="1" offset-md="4">
-              <v-text-field
-                v-model="book.imported_quantity"
-                hide-details
-                :min="0"
-                single-line
-                type="number"
-              />
+            <v-col md="4">
+              <v-text-field v-model="book.local_quantity" label="Local" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
             </v-col>
           </v-row>
           <v-row class="mb-6" no-gutters style="margin-top: -1rem">
             <v-col md="4">
-              <v-checkbox v-model="book.local" :label="`Local`"></v-checkbox>
+              <v-text-field v-model="book.imported_quantity" label="Imported" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
             </v-col>
-            <v-col md="1" offset-md="4">
-              <v-text-field
-                v-model="book.local_quantity"
-                hide-details
-                single-line
-                :min="0"
-                type="number"
-              />
+          </v-row>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="3">
+              <v-text-field v-model="book.price" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="3">
+              <v-text-field v-model="book.importedPrice" :rules="priceRules" label="€"></v-text-field>
             </v-col>
           </v-row>
         </v-col>
       </v-row>
     </v-container>
+
+     <!-- Input Container for Music CD -->
+    <v-container class="grey lighten-2" style="height: 11rem;margin-top: 1rem;width: 70%">
+      <v-row class="mb-6" no-gutters>
+        <v-col md="2">
+          <h2 style="margin-top: 3rem">Music CD</h2>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="4">
+              <v-text-field v-model="musicCd.local_quantity" label="Local" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="4">
+              <v-text-field v-model="musicCd.imported_quantity" label="Imported" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="3">
+              <v-text-field v-model="musicCd.price" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="3">
+              <v-text-field v-model="musicCd.importedPrice" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- Input Container for Chocolate -->
+    <v-container class="grey lighten-2" style="height: 11rem;margin-top: 1rem;width: 70%">
+      <v-row class="mb-6" no-gutters>
+        <v-col md="2">
+          <h2 style="margin-top: 3rem">Chocolate</h2>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="4">
+              <v-text-field v-model="chocolate.local_quantity" label="Local" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="4">
+              <v-text-field v-model="chocolate.imported_quantity" label="Imported" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="3">
+              <v-text-field v-model="chocolate.price" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="3">
+              <v-text-field v-model="chocolate.importedPrice" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- Input Container for Chocolate -->
+    <v-container class="grey lighten-2" style="height: 11rem;margin-top: 1rem;width: 70%">
+      <v-row class="mb-6" no-gutters>
+        <v-col md="2">
+          <h2 style="margin-top: 3rem">Perfume</h2>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="4">
+              <v-text-field v-model="perfume.local_quantity" label="Local" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="4">
+              <v-text-field v-model="perfume.imported_quantity" label="Imported" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="3">
+              <v-text-field v-model="perfume.price" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="3">
+              <v-text-field v-model="perfume.importedPrice" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- Input Container for Headache Pills -->
+    <v-container class="grey lighten-2" style="height: 11rem;margin-top: 1rem;width: 70%">
+      <v-row class="mb-6" no-gutters>
+        <v-col md="2">
+          <h2 style="margin-top: 3rem">Headache Pills</h2>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="4">
+              <v-text-field v-model="headachePills.local_quantity" label="Local" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="4">
+              <v-text-field v-model="headachePills.imported_quantity" label="Imported" :rules="priceRules" outlined :min="0"
+                type="number" clearable></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col md="4">
+          <v-row class="mb-6" no-gutters>
+            <v-col md="3">
+              <v-text-field v-model="headachePills.price" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mb-6" no-gutters style="margin-top: -1rem">
+            <v-col md="3">
+              <v-text-field v-model="headachePills.importedPrice" :rules="priceRules" label="€"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Main",
-
+  components: {},
   data() {
     return {
+      priceRules: [(v) => v >= 0],
       book: {
-        price:'€12.49',
+        price: "",
+        importedPrice: "",
+        imported: false,
+        imported_quantity: 0,
+        local: false,
+        local_quantity: 0,
+      },
+      musicCd: {
+        price: "",
+        importedPrice: "",
+        imported: false,
+        imported_quantity: 0,
+        local: false,
+        local_quantity: 0,
+      },
+      chocolate: {
+        price: "",
+        importedPrice: "",
+        imported: false,
+        imported_quantity: 0,
+        local: false,
+        local_quantity: 0,
+      },
+      chocolate: {
+        price: "",
+        importedPrice: "",
+        imported: false,
+        imported_quantity: 0,
+        local: false,
+        local_quantity: 0,
+      },
+      perfume: {
+        price: "",
+        importedPrice: "",
+        imported: false,
+        imported_quantity: 0,
+        local: false,
+        local_quantity: 0,
+      },
+
+      headachePills: {
+        price: "",
+        importedPrice: "",
         imported: false,
         imported_quantity: 0,
         local: false,
@@ -82,5 +254,13 @@ export default {
       checkbox: true,
     };
   },
+  methods:{
+    
+  }
 };
 </script>
+<style lang="scss">
+.v-input__icon--clear {
+  display: none;
+}
+</style>
